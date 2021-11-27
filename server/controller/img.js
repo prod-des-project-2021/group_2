0abc09fc7a9b84b3f1ac5ad00151fd1b925cf780
Quoc Huy <quochuy270800@gmail.com>
2021-11-27 23:37:00 +0200

    add addCourse api

--- server/controller/img.js
+++ server/controller/img.js
@@ -3,31 +3,32 @@ const cloudinary = require('../middleware/cloudinary')
 const { v4: uuidv4 } = require('uuid')
 
 const addImage = async (req, res, next) => {
-  const uploadedImage = await cloudinary.uploader.upload(
-    req.file.path,
-    { public_id: `testing/${uuidv4()}`, tags: `testing` },
-    function (err, image) {
-      if (err) return res.send(err)
-      console.log(image)
-      console.log('Uploaded Cloudinary successfully')
-    },
-  )
-
+  // const uploadedImage = await cloudinary.uploader.upload(
+  //   req.file.path,
+  //   { public_id: `course/${uuidv4()}`, tags: `course` },
+  //   function (err, image) {
+  //     if (err) return res.send(err)
+  //     console.log('Uploaded Cloudinary successfully')
+  //   },
+  // )
   let image = new Images({
     alt: req.body.alt,
-    image_link: req.body.image_link,
+    image_link: req.file.path,
     title: req.body.title,
     news: req.body.news,
     link: req.body.link,
   })
+  
   image
     .save()
     .then((result) => {
+      console.log(result.image_link)
       res.json({
         message: 'Image added successfully',
       })
     })
     .catch((err) => {
+      console.log(err)
       res.json({
         message: 'Image add failed',
       })
